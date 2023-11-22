e8c44f17c37f2c475424f5e292c9802952f56672
uvsq22201832 <114344454+uvsq22201832@users.noreply.github.com>
2023-11-22 09:04:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,136 +2,43 @@ import json
 import re
 import pandas as pd
 import textblob
-import matplotlib.pyplot as plt
-import random
+
+df = pd.read_json("aitweets.json")
 
 
-
-with open("aitweets.json", "r", encoding="utf-8") as f:
-    tweets = json.load(f)
-
-auteur = ["TwitMaster23",
-    "Tweetophile",
-    "CoeurDeTweeter",
-    "ChroniqueurTweets",
-    "FlècheDeTweet",
-    "Pionnel Pessi",
-    "Leize.py",
-    "ÉchoTweeter",
-    "Dimitri.lasdeg",
-    "PixelTweeter",
-    "Yanis180",
-    "NuageDeMots",
-    "RésonanceTwitter",
-    "HarmonieDeTweets",
-    "ÉclatDePlume",
-    "TempêteDeMots",
-    "FusionTweets",
-    "CascadeDeTweets",
-    "PoésieDePixels",
-    "ZénithTweeter",
-    "ÉclipseDeMots",
-    "AstreDeTweets",
-    "SymphonieTwitter",
-    "LueurDePlume",
-    "VirtuoseDeTweets",
-    "SillageDeMots",
-    "PépiteTweeter",
-    "OrageDeTweets",
-    "PapillonDeMots",
-    "RhapsodieTwitter",
-    "MirageDeTweets",
-    "EssenceDePlume",
-    "OndeTweeter",
-    "CascadeDeMots",
-    "BrumeDeTweets",
-    "MélopéeTwitter",
-    "ÉclatDeTweeter",
-    "EncreDeMots",
-    "FlammeDeTweets",
-    "TrésorDePlume",
-    "AuroreTweeter",
-    "MystèreDeMots",
-    "LumièreTwitter",
-    "RivièreDeTweets",
-    "VagueDePlume",
-    "ÉtoileTweeter",
-    "ÉnigmeDeTweets",
-    "BrisDeMots",
-    "InspirationTwitter",
-    "SoleilDeTweets",
-    "LabyrintheDePlume",
-    "RefletTweeter",
-    "OasisDeMots",
-    "ÉphémèreTwitter",
-    "CristalDeTweets",
-    "PassionDePlume",
-    "RafaleTweeter",
-    "CharmeDeMots",
-    "ÉclipsTwitter",
-    "CâlinDeTweets",
-    "ÉmeraudeDePlume",
-    "ÉchoTweeter",
-    "AileDeMots",
-    "LibelluleTwitter",
-    "CaresseDeTweets",
-    "SérénitéDePlume",
-    "ÉtoileFilanteTweeter",
-    "SongeDeMots",
-    "ÉveilTwitter",
-    "GazouillisDeTweets",
-    "DouceurDePlume",
-    "TempêteTweeter",
-    "ÉvasionDeMots",
-    "LueurTwitter",
-    "ArcEnCielDeTweets",
-    "InspirationDePlume",
-    "RafaleTwitter",
-    "OdysséeDeMots",
-    "ÉclatTwitter",
-    "GoutteDeTweets",
-    "MerveilleDePlume",
-    "OrageTwitter",
-    "RêveDeMots",
-    "HorizonDeTweets",
-    "VibrationDePlume",
-    "BriseTwitter",
-    "MélodieDeTweets",
-    "SérénadeDePlume",
-    "CascadeTwitter",
-    "ÉlixirDeMots",
-    "RécitDeTweeter",
-    "AubeTwitter",
-    "EssenceDePlume",
-    "ÉchoDeMots",
-    "SouffleTweeter",
-    "LueurDePlume",
-    "ÉpisodeTwitter",
-    "NuageDeMots",
-    "InspirationDeTweeter",
-    "VortexTwitter"
-    ]
-
-for tweet in tweets:
-    tweet["Author"] = random.choice(auteur)
-
-with open('aitweets.json', 'w', encoding='utf-8') as f:
-    json.dump(tweets, f, ensure_ascii=False, indent=2)
 
 # Assurez-vous d'avoir déjà chargé vos données dans un DataFrame, par exemple, df.
-df = pd.read_json("aitweets.json")
+
 def topk_hashtag(k):
     hashtags_freq = {}  # Dictionnaire pour stocker la fréquence des hashtags
 
     for tweet_text in df["TweetText"]:
         hashtags = re.findall(r'#\w+', tweet_text)  # Trouver tous les hashtags dans le texte
         for hashtag in hashtags:
-            # Convertir en minuscules pour compter de manière insensible à la casse
+            hashtag = hashtag.lower()  # Convertir en minuscules pour compter de manière insensible à la casse
             if hashtag in hashtags_freq:
                 hashtags_freq[hashtag] += 1
             else:
                 hashtags_freq[hashtag] = 1
 
+
+def topKmentionne(k):
+    dic={}
+    for tweet in df["TweetText"]:
+        arobases = re.findall(r'#\@+', tweet)
+        for arobase in arobases:
+            arobase = arobase.lower()  # Convertir en minuscules pour compter de manière insensible à la casse
+            if arobase in dic:
+                dic[arobase] += 1
+            else:
+                dic[arobase] = 1
+    sorted_arobases = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+    top_k_arobases = sorted_arobases[:k]
+    for arobase, count in top_k_arobases:
+        print(f"Hashtag : {arobase}, Fréquence : {count}")
+        
+print(topKmentionne(int(input("Top K arobase: "))))
+    
     # Trier les hashtags par fréquence décroissante
     sorted_hashtags = sorted(hashtags_freq.items(), key=lambda x: x[1], reverse=True)
 
@@ -174,13 +81,7 @@ def find_user_hashtags(hashtag):
             print(hashtag)
             print(tweet)
 find_user_mention("#hdatasystems")
-     
-taille = len(df)
-print(taille)
 
-print(df)
-
-    
 
 def sentiment(id):
     s = df.loc[df['id'] == id]
